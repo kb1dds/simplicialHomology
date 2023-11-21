231cfb9e78ac838cef50947f08bc6a609421e141
Michael Robinson <kb1dds@gmail.com>
2023-11-21 15:29:14 -0500

    Update simplicialHomology.py

--- simplicialHomology.py
+++ simplicialHomology.py
@@ -1,9 +1,18 @@
 # Relative simplicial homology library
 #
 # Copyright (c) 2014-2019, Michael Robinson, Chris Capraro
-# Distribution of unaltered copies permitted for noncommercial use only
-# All other uses require express permission of the author
-# This software comes with no warrantees express or implied 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import numpy as np
 import itertools as it
